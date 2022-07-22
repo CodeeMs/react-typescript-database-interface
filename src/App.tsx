375dff0d388224a9aaefc1adfddf0dc231394ef0
CodeeMs <webmaster777pamn@protonmail.com>
2022-07-22 16:01:54 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { LogContext } from './context/context';
+import Main from './pages/Main';
+import './style/main.css';
 
-function App() {
+const App = () => {
+  const [isLogedIn, setIsLogedIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LogContext.Provider value={{
+      isLogedIn,
+      setIsLogedIn
+    }}>
+     <Main/>
+    </LogContext.Provider>
   );
-}
+};
 
 export default App;
